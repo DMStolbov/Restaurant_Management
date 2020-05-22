75d8e6ccb030519e8fc51ce18a60d3419a1511e4
DStolbov <dmstolbov@edu.hse.ru>
2020-05-22 20:16:22 +0300

    Testing push

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-# First version
-# Testing our repository
-
 print("Hello my friends. I am Nastya")
 
 print("Yeah, Now I know how to use git!!!!AHAHAHAHAHHA")
+
+# Hi buddies, Dan here! How are you doing?
+# I'm testing how push works from PyCharm
+
